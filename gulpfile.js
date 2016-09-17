5f2d08799a2c3c1aba2919559872ca636040670e
Hunter Caron <huntercaron@me.com>
2016-09-17 14:36:31 -0400

    adding browsersync to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ gulp.task("build-js", function() {
 });
 
 gulp.task("watch", function() {
+    gulp.watch("public/*.html", ["reload"]);
     gulp.watch("public/css/*.css", ["reload"]);
     gulp.watch("public/**/*.js", ["build-js"]);
 });
