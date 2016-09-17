03438ab374b1e4a7c53a2bf44a9567e51527c589
Hunter Caron <huntercaron@me.com>
2016-09-17 17:08:25 -0400

    removing browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var babel = require("gulp-babel");
 var fs = require('fs');
 var browserify = require('browserify');
 var babelify = require('babelify');
-var browserSync = require('browser-sync').create();
 
 gulp.task("build-js", function() {
     browserify(["./public/js/index.js"])
@@ -15,18 +14,4 @@ gulp.task("build-js", function() {
 
 gulp.task("watch", function() {
     gulp.watch("public/**/*.js", ["build-js"]);
-
-    gulp.watch("public/*.html", ["reload"]);
-    gulp.watch("public/css/*.css", ["reload"]);
-    gulp.watch("public/js/bundle.js", ["reload"]);
-});
-
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        proxy: "localhost:3000"
-    });
-});
-
-gulp.task("reload", function() {
-    browserSync.reload();
 });
