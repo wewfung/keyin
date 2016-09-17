1c404c0c556c378fd8531905f0f437f36c469e8b
William Fung <hellox56@gmail.com>
2016-09-17 04:45:37 -0400

    added select populated by js file

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+var selectDataset = document.getElementById('box');
+
+for(var i = 0; i < 4; i++){
+	var option = document.createElement('option');
+	option.value = i;
+	option.innerHTML = "WKEJLRKJ";
+	selectDataset.appendChild(option);
+}
