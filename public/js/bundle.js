a2d79eb7f04b58e05ff7bed79f7b771613a75e7e
Hunter Caron <huntercaron@me.com>
2016-09-17 13:05:51 -0400

    stuff is broken

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,7 +1,48 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.getDataSetById = getDataSetById;
+exports.searchForDataSets = searchForDataSets;
+function getDataSetById(id, version) {
+	return new Promise(function (resolve, reject) {
+		var url = '/getdataset/' + id + '?version=' + version;
+		var request = new XMLHttpRequest();
+		request.open('GET', url, true);
+		request.onload = function () {
+			resolve(JSON.parse(request.response));
+		};
+		request.onerror = function () {
+			reject(new Error("Error retrieving data set"));
+		};
+		request.send();
+	});
+}
+
+function searchForDataSets(searchString) {
+	return new Promise(function (resolve, reject) {
+		var url = '/searchfordatasets';
+		var request = new XMLHttpRequest();
+		request.open('POST', url, true);
+		request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		request.onload = function () {
+			resolve(JSON.parse(request.response));
+		};
+		request.onerror = function () {
+			reject(new Error("Error retrieving data set"));
+		};
+		request.send('query=' + searchString);
+	});
+}
+
+},{}],2:[function(require,module,exports){
 "use strict";
 
 var _shit = require("./lib/shit");
+
+var _getdatasets = require("./getdatasets.js");
 
 var _graph = require("./lib/graph");
 
@@ -9,16 +50,16 @@ var _graph2 = _interopRequireDefault(_graph);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// Importing Single Functions
 (0, _shit.shit)();
 
 // Importing Classes
+// Importing Single Functions
 
 (0, _shit.otherShit)();
 
 var graph = new _graph2.default({ height: 10 });
 
-},{"./lib/graph":2,"./lib/shit":3}],2:[function(require,module,exports){
+},{"./getdatasets.js":1,"./lib/graph":3,"./lib/shit":4}],3:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -35,7 +76,7 @@ var Graph = function Graph(args) {
 
 exports.default = Graph;
 
-},{}],3:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -51,4 +92,4 @@ function otherShit() {
     console.log("other shitty");
 }
 
-},{}]},{},[1]);
+},{}]},{},[2]);
