51229ba427431fc8ffcd26742672a1105829e91b
Hunter Caron <huntercaron@me.com>
2016-09-17 14:42:37 -0400

    stopping eric's api madness

--- public/js/index.js
+++ public/js/index.js
@@ -8,8 +8,8 @@ import Graph from './lib/graph';
 shit();
 otherShit();
 
-searchForDataSets().then(data => console.log(data)).catch(err => console.log(err));
-searchForDataSets('').then(data => console.log(data)).catch(err => console.log(err));
-getDataSetById('4d4418ca-ce52-465c-8a84-f11260c7da92', 1).then(data => console.log('data' + data));
+// searchForDataSets().then(data => console.log(data)).catch(err => console.log(err));
+// searchForDataSets('').then(data => console.log(data)).catch(err => console.log(err));
+// getDataSetById('4d4418ca-ce52-465c-8a84-f11260c7da92', 1).then(data => console.log('data' + data));
 
 const graph = new Graph({height: 10});
