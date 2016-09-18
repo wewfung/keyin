306e71786a24db309a3fedd206a4a7ee6acfac88
William Fung <hellox56@gmail.com>
2016-09-18 03:19:19 -0400

    tweak graph sizes

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -2,6 +2,7 @@ export function makeGraph() {
 
 		var keyX = $('#htnX').val();
 		var keyY = $('#htnY').val();
+		//var keyValue = $('#htnValue').val();
 
 		var arrX = [];
 		var arrY = [];
@@ -15,17 +16,16 @@ export function makeGraph() {
 
 		var divCharts = $('#chartSection');
 		divCharts.empty();
-		divCharts.append($('<canvas id="myChart" width="80%" height="60%"></canvas>'));
-		divCharts.append($('<canvas id="myLineChart" width="80%" height="60%"></canvas>'));
+		divCharts.append($('<canvas id="myChart" width="600" height="400"></canvas>'));
+		divCharts.append($('<canvas id="myLineChart" width="600" height="400"></canvas>'));
+		var ctx = document.getElementById("myChart");
 		var ctx = document.getElementById("myChart");
 		var myChart = new Chart(ctx, {
 				type: 'bar',
 				data: {
-						//labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
 						labels: arrX,
 						datasets: [{
 								label: keyY,
-								//data: [12, 19, 3, 5, 2, 3],
 								data: arrY,
 								backgroundColor: colors,
 								borderColor: borderColors,
@@ -42,21 +42,18 @@ export function makeGraph() {
 		var myLineChart = new Chart(ctlx, {
 				type: 'line',
 				data: {
-						//labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
 						labels: arrX,
 						datasets: [{
 								label: keyY,
-								//data: [12, 19, 3, 5, 2, 3],
 								data: arrY,
 								backgroundColor: colors,
 								borderColor: borderColors,
 								borderWidth: 1
-						}],
-						options: {
-							responsive: false,
-							maintainAspectRatio: false
-						}
-								
+						}]							
+				},
+				options: {
+					responsive: false,
+					maintainAspectRatio: false
 				}
 		});
 }
